124a819421a283dcbf206a9083325313b4cdc18c
LordCookis <azat525210@gmail.com>
2023-08-01 23:12:45 +0600

    add DEATH

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,22 @@ import { useState } from 'react'
 
 export default function Home() {
   const [field, setField] = useState<any>([])
-  const [sizeX, setSizeX] = useState<number>()
-  const [sizeY, setSizeY] = useState<number>()
+  const [sizeX, setSizeX] = useState<number>(0)
+  const [sizeY, setSizeY] = useState<number>(0)
 
   const createField = (e: any) => {
     e.preventDefault()
-    let fieldX:any = []
-    let idX:number
-    let idY:number
-    for (idX = 0; idX < sizeX; idX++) {
+    setField([])
+    const fieldX:any = []
+    let idX:number = 0
+    let idY:number = 0
+    for (let i = 0; i < sizeX; i++) {
       const arrayX:any = []
       for (let j = 0; j < sizeY; j++) {
         arrayX.push({
           id: idY,
-          bomb: true
+          bomb: Boolean(randomBomb()),
+          click: false,
         })
         idY++
       }
@@ -23,8 +25,24 @@ export default function Home() {
         id: idX,
         array: arrayX
       })
+      idX++
     }
     setField(fieldX)
+    console.log(fieldX)
+  }
+
+  const randomBomb = () => {
+    const bomb = Math.random()
+    return bomb < 0.1 ? 1 : 0
+  }
+
+  const cellClick = (idX: number, id: number) => {
+    const fieldX = [...field]
+    fieldX[idX].array[id].click = true
+    setField(fieldX)
+    if (field[idX].array[id].bomb === true) {
+      fieldX.map((arrayX: any) => arrayX.array.map((cellX: any) => cellX.bomb ? cellX.click = true : null))
+    }
   }
 
   return (
@@ -39,14 +57,15 @@ export default function Home() {
         <button className='gameButton'>НАЧАТЬ ИГРУ</button>
       </form>
       <div className='fieldDiv'>
-        {field.map((cellX:any)=>(
+        {field?.map((cellX:any, indexX:number)=>(
           <div className='filedCellX' key={cellX.id}>
-          {cellX.array.map((cell:any)=>(
-            <div className='fieldCell' key={cell.id}></div>
+          {cellX?.array?.map((cell:any, index: number)=>(
+            <button className={`${cell.click && cell.bomb ? 'bomb' : ''}`} key={cell.id} onClick={()=>cellClick(indexX, index)}></button>
           ))}
           </div>
         ))}
       </div>
+      <span className='span'>ТЫ СДОХ НАХУЙ, АМЕРИКА ЗАХУЯРИЛА ТЕБЯ ЯДЕРКОЙ</span>
     </div>
   )
 }
