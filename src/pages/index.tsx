4fff834848e9f0ef38c00f09df3769acedc6304b
LordCookis <azat525210@gmail.com>
2023-08-02 15:20:18 +0600

    add countBomb

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ export default function Home() {
           id: idY,
           bomb: Boolean(randomBomb()),
           click: false,
+          countBomb: 0
         })
         idY++
       }
@@ -37,12 +38,49 @@ export default function Home() {
   }
 
   const cellClick = (idX: number, id: number) => {
+    let countBomb = 0
     const fieldX = [...field]
     fieldX[idX].array[id].click = true
-    setField(fieldX)
+    
     if (field[idX].array[id].bomb === true) {
       fieldX.map((arrayX: any) => arrayX.array.map((cellX: any) => cellX.bomb ? cellX.click = true : null))
+    } else {
+      if (field[idX-1]?.array[id-1]?.bomb === true) {
+        countBomb++
+        console.log(111, idX-1, id-1)
+      }
+      if (field[idX-1]?.array[id]?.bomb === true) {
+        countBomb++
+        console.log(222, idX-1, id)
+      }
+      if (field[idX-1]?.array[id+1]?.bomb === true) {
+        countBomb++
+        console.log(333, idX-1, id+1)
+      }
+      if (field[idX]?.array[id-1]?.bomb === true) {
+        countBomb++
+        console.log(444, idX, id-1)
+      }
+      if (field[idX]?.array[id+1]?.bomb === true) {
+        countBomb++
+        console.log(555, idX, id+1)
+      }
+      if (field[idX+1]?.array[id-1]?.bomb === true) {
+        countBomb++
+        console.log(666, idX+1, id-1)
+      }
+      if (field[idX+1]?.array[id]?.bomb === true) {
+        countBomb++
+        console.log(777, idX+1, id)
+      }
+      if (field[idX+1]?.array[id+1]?.bomb === true) {
+        countBomb++
+        console.log(888, idX+1, id+1)
+      }
+      fieldX[idX].array[id].countBomb = countBomb
     }
+    setField(fieldX)
+    console.log(fieldX)
   }
 
   return (
@@ -60,7 +98,16 @@ export default function Home() {
         {field?.map((cellX:any, indexX:number)=>(
           <div className='filedCellX' key={cellX.id}>
           {cellX?.array?.map((cell:any, index: number)=>(
-            <button className={`${cell.click && cell.bomb ? 'bomb' : ''}`} key={cell.id} onClick={()=>cellClick(indexX, index)}></button>
+            <button className={`${cell.click && cell.bomb ? 'bomb' : ''} 
+            ${cell.click && cell.countBomb === 1 ? 'one' : 
+            cell.click && cell.countBomb === 2 ? 'two' : 
+            cell.click && cell.countBomb === 3 ? 'three' : 
+            cell.click && cell.countBomb === 4 ? 'four' : 
+            cell.click && cell.countBomb === 5 ? 'five' : 
+            cell.click && cell.countBomb === 6 ? 'six' : 
+            cell.click && cell.countBomb === 7 ? 'seven' : 
+            cell.click && cell.countBomb === 8 ? 'eight' : 
+            null}`} key={cell.id} onClick={()=>cellClick(indexX, index)}>{cell.countBomb}</button>
           ))}
           </div>
         ))}
